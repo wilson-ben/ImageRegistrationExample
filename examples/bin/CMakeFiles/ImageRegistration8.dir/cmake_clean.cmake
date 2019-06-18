57f1ddfe1f4759f6452bf173e04e35a79e6464a3
Ben Wilson <ben.wilson@kitware.com>
2019-06-18 10:56:52 -0400

    Initalize ImageExample

---
+++ examples/bin/CMakeFiles/ImageRegistration8.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ImageRegistration8.dir/ImageRegistration8.cxx.o"
+  "ImageRegistration8.pdb"
+  "ImageRegistration8"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ImageRegistration8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
